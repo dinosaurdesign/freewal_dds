c5ebf43aa36ca72a2060c90f93eefebbd7f1c5cf
Alexander Kulnyow <alexanderkulnyow@yahoo.com>
2018-01-19 15:56:38 +0300

    init

--- src/plugins/MixItUp.php
+++ src/plugins/MixItUp.php
@@ -10,28 +10,29 @@
                 </ul>
             </div>
             <div class="container1">
-                <div class="mix green"><img class="miximg"
+
+                <div class="mix green"><img class="miximg img-fluid"
                             src="https://image.shutterstock.com/z/stock-photo-handsome-man-in-spectacles-portrait-606177383.jpg"
-                            alt=""></div>
-                <div class="mix green"><img class="miximg"
+                             alt=""></div>
+                <div class="mix green"><img class="miximg img-fluid"
                             src="https://image.shutterstock.com/z/stock-photo-close-up-portrait-of-a-professional-business-woman-smiling-outdoor-287720834.jpg"
                             alt=""></div>
-                <div class="mix blue"><img class="miximg"
+                <div class="mix blue"><img class="miximg img-fluid"
                             src="https://image.shutterstock.com/z/stock-photo-indoor-portrait-of-beautiful-brunette-young-dark-skinned-woman-with-shaggy-hairstyle-smiling-640005220.jpg"
                             alt=""></div>
-                <div class="mix pink"><img class="miximg"
+                <div class="mix pink"><img class="miximg img-fluid"
                             src="https://image.shutterstock.com/z/stock-photo-portrait-of-a-mature-man-smiling-at-the-camera-522085669.jpg"
                             alt=""></div>
-                <div class="mix green"><img class="miximg"
+                <div class="mix green"><img class="miximg img-fluid"
                             src="https://image.shutterstock.com/z/stock-photo-young-smiling-woman-portrait-positive-emotions-617698403.jpg"
                             alt=""></div>
-                <div class="mix blue"><img class="miximg"
+                <div class="mix blue"><img class="miximg img-fluid"
                             src="https://image.shutterstock.com/z/stock-photo-funny-woman-on-grey-background-cheerful-female-model-joyful-positive-human-emotion-facial-396642952.jpg"
                             alt=""></div>
-                <div class="mix pink"><img class="miximg"
+                <div class="mix pink"><img class="miximg img-fluid"
                             src="https://image.shutterstock.com/z/stock-photo-horizontal-portrait-of-pleasant-looking-caucasian-female-with-long-hair-pink-on-tips-having-696230005.jpg"
                             alt=""></div>
-                <div class="mix blue"><img class="miximg"
+                <div class="mix blue"><img class="miximg img-fluid"
                             src="https://image.shutterstock.com/z/stock-photo-close-up-portrait-of-a-beautiful-young-african-american-woman-smiling-233378335.jpg"
                             alt=""></div>
             </div>
